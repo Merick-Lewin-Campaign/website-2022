caf7cec6966a74debdd288bb76a92e698263470a
dlongo <dlongodesign@gmail.com>
2022-05-29 20:51:22 -0400

    add about section and responsive styling

--- dist/index.975ef6c8.js
+++ dist/index.975ef6c8.js
@@ -504,6 +504,14 @@ function hmrAcceptRun(bundle, id) {
 
 },{}],"8lqZg":[function(require,module,exports) {
 var _bootstrap = require("bootstrap");
+//homepage nav background color when begin to scroll down
+const navbar = document.querySelector("nav");
+window.onscroll = function() {
+    // pageYOffset or scrollY
+    if (window.pageYOffset > 50) navbar.classList.add("scrolled");
+    else navbar.classList.remove("scrolled");
+} // underline animation when scroll reach the section
+;
 
 },{"bootstrap":"h36JB"}],"h36JB":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import 'bootstrap';
+
+//homepage nav background color when begin to scroll down
+const navbar = document.querySelector('nav')
+
+window.onscroll = function () {
+    // pageYOffset or scrollY
+    if (window.pageYOffset > 50) {
+        navbar.classList.add('scrolled')
+    } else {
+        navbar.classList.remove('scrolled')
+    }
+}
+
+// underline animation when scroll reach the section
